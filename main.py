c3f6c15ee565aa5449235caf103ee7bfaf5be132
Slava <slavag119@gmail.com>
2024-05-29 12:18:02 +0400

    Start commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import pyautogui
+from time import sleep
+
+# print(pyautogui.position())
+
+# print(pyautogui.size())
+
+# print(pyautogui.onScreen(2000, 600)
+
+pyautogui.FAILSAFE = False
+
+# pyautogui.moveTo(0, 0)
+# pyautogui.PAUSE = 2
+# pyautogui.moveTo(1200, 500)
+# pyautogui.PAUSE = 2
+# pyautogui.moveTo(500, 500)
+
+# pyautogui.moveRel(100, 100, 2)
+
+# pyautogui.click(200, 200, 2, 2, 'left')
+
+# x, y = 120, 50
+#
+# pyautogui.rightClick(x, y)
+# pyautogui.doubleClick(x, y)
+# pyautogui.tripleClick(x, y)
+# pyautogui.middleClick(x, y)
+
+# pyautogui.mouseDown(100, 500, 'left')
+# pyautogui.mouseUp(300, 500, 'left')
+
+# pyautogui.scroll(-1000, 300, 300)
+# pyautogui.scroll(1000, 300, 300)
+
+# print(pyautogui.KEYBOARD_KEYS)
+
+# sleep(1)
+#
+# pyautogui.typewrite("I'm always come back!", 0.3)
+# pyautogui.typewrite(['p', 'y', 't', 'h', 'o', 'n', '!', 'backspace', 'enter', 'G', 'U', 'I'], 0.2)
+
+# pyautogui.hotkey('shift', 'enter')
+
+# pyautogui.screenshot('name.png')
+
+# pyautogui.confirm('Готовы?')
+# pyautogui.alert('Ошибка!')
+# pyautogui.prompt('Ваше имя: ')
